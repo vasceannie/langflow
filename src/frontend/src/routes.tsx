3eb7c29c5d9ee39f657b760a126acf59928ba0d4
Gabriel Luiz Freitas Almeida <gabriel@langflow.org>
2024-09-24 18:21:08 -0300

    chore: Add route for tasks page in router configuration

--- src/frontend/src/routes.tsx
+++ src/frontend/src/routes.tsx
@@ -154,6 +154,7 @@ const router = createBrowserRouter(
                     </ProtectedAdminRoute>
                   }
                 />
+                <Route path="tasks" element={<TaskPage />} />
               </Route>
               <Route path="flow/:id/">
                 <Route path="" element={<DashboardWrapperPage />}>
@@ -191,7 +192,6 @@ const router = createBrowserRouter(
               </ProtectedLoginRoute>
             }
           />
-          <Route path="/tasks" element={<TaskPage />} />
         </Route>
       </Route>
       <Route path="*" element={<CustomNavigate replace to="/" />} />
